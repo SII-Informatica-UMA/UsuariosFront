295c4d6174cb4db72f112c884431caf203150241
carlosspino <carlospino@uma.es>
2023-05-18 20:00:16 +0200

    tests implementados

--- src/app/components/editar-usuario/editar-usuario.component.spec.ts
+++ src/app/components/editar-usuario/editar-usuario.component.spec.ts
@@ -1,21 +1,74 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Location } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
 import { EditarUsuarioComponent } from './editar-usuario.component';
+import { UsuariosService } from '../../services/usuarios.service';
+import { Usuario } from '../../models/usuario';
+import { of, throwError } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 describe('EditarUsuarioComponent', () => {
   let component: EditarUsuarioComponent;
   let fixture: ComponentFixture<EditarUsuarioComponent>;
+  let usuariosServiceSpy: jasmine.SpyObj<UsuariosService>;
+  let routerSpy: jasmine.SpyObj<Router>;
 
   beforeEach(() => {
+    const usuariosService = jasmine.createSpyObj('UsuariosService', ['getUsuarioById', 'updateUsuario']);
+    const router = jasmine.createSpyObj('Router', ['navigate']);
+
     TestBed.configureTestingModule({
-      declarations: [EditarUsuarioComponent]
-    });
+      declarations: [EditarUsuarioComponent],
+      imports: [FormsModule],
+      providers: [
+        Location,
+        { provide: ActivatedRoute, useValue: { snapshot: { params: { id: 1 } } } },
+        { provide: UsuariosService, useValue: usuariosService },
+        { provide: Router, useValue: router }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(EditarUsuarioComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    usuariosServiceSpy = TestBed.inject(UsuariosService) as jasmine.SpyObj<UsuariosService>;
+    routerSpy = TestBed.inject(Router) as jasmine.SpyObj<Router>;
   });
 
-  it('should create', () => {
+  it('debería crearse', () => {
     expect(component).toBeTruthy();
   });
+
+  it('debería obtener el usuario al inicializarse', () => {
+    const usuario: Usuario = {
+      id: 1,
+      nombre: 'Juan',
+      apellido1: 'Perez',
+      apellido2: 'Gallardo',
+      email: 'jpegallardo@example.com',
+      dni: '123456789',
+      username: 'juanpe',
+      telefono: '1234567890'
+    };
+    usuariosServiceSpy.getUsuarioById.and.returnValue(of(usuario));
+
+    component.ngOnInit();
+
+    expect(usuariosServiceSpy.getUsuarioById).toHaveBeenCalledWith(1);
+    expect(component.usuario).toEqual(usuario);
+  });
+
+  it('Debería mostrar una alerta al haber error al edita el usuario', () => {
+    const errorMessage = 'Error al editar el usuario.';
+    usuariosServiceSpy.updateUsuario.and.returnValue(throwError({ error: errorMessage }));
+
+    spyOn(window, 'alert');
+    spyOn(console, 'error');
+
+    component.onUpdate();
+
+    expect(window.alert).toHaveBeenCalledWith('Error al editar el usuario ' + errorMessage);
+    expect(console.error).toHaveBeenCalled();
+  });
+
+
 });
--- src/app/components/lista-usuarios/lista-usuarios.component.spec.ts
+++ src/app/components/lista-usuarios/lista-usuarios.component.spec.ts
@@ -1,21 +1,69 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Observable, of } from 'rxjs';
 
 import { ListaUsuariosComponent } from './lista-usuarios.component';
+import { UsuariosService } from '../../services/usuarios.service';
+import { Usuario } from '../../models/usuario';
 
 describe('ListaUsuariosComponent', () => {
   let component: ListaUsuariosComponent;
   let fixture: ComponentFixture<ListaUsuariosComponent>;
+  let usuariosService: UsuariosService;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ListaUsuariosComponent],
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      providers: [UsuariosService]
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ListaUsuariosComponent]
-    });
     fixture = TestBed.createComponent(ListaUsuariosComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    usuariosService = TestBed.inject(UsuariosService);
   });
 
-  it('should create', () => {
+  it('debe crear la componente', () => {
     expect(component).toBeTruthy();
   });
+
+  it('debe devolver la lista de usuarios cuando se inicializa', () => {
+    const mockUsuarios: Usuario[] = [
+      new Usuario('John', 'Doe', 'Doe', 'john@example.com', 'johndoe', '123456789', '555-1234'),
+      new Usuario('Jane', 'Smith', 'Smith', 'jane@example.com', 'janesmith', '987654321', '555-5678'),
+      new Usuario('Michael', 'Johnson', 'Johnson', 'michael@example.com', 'michaeljohnson', '567890123', '555-9876'),
+    ];
+
+    spyOn(usuariosService, 'getAllUsuarios').and.returnValue(of(mockUsuarios));
+
+    fixture.detectChanges();
+
+    expect(component.listaUsuarios).toEqual(mockUsuarios);
+  });
+
+  it('debe borrar un usuario cuando eliminarUsuario es llamado', () => {
+    const mockUserId = 1;
+    spyOn(usuariosService, 'deleteUsuario').and.returnValue(of({}));
+
+    spyOn(component, 'obtenerUsuarios');
+
+    component.eliminarUsuario(mockUserId);
+
+    expect(usuariosService.deleteUsuario).toHaveBeenCalledWith(mockUserId);
+    expect(component.obtenerUsuarios).toHaveBeenCalled();
+  });
+
+  it('Debe navegar a add-usuario cuando addUsuario es llamado', () => {
+    spyOn(component['router'], 'navigate');
+
+    component.addUsuario();
+
+    expect(component['router'].navigate).toHaveBeenCalledWith(['add-usuario']);
+  });
+
+  // Add more test cases as needed
+
 });
